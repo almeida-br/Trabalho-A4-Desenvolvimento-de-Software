7a0e9d6659369a532ff9d7c2354852902ea58d0f
samara <samaracostad9@gmail.com>
2024-06-07 10:46:34 -0300

    Co-authored-by: ka-rodrigues <rodrigues-br@users.noreply.github.com> Co-authored-by: Laila Fonseca <BlackStar07@users.noreply.github.com> Co-authored-by: Kau002 <Kau002@users.noreply.github.com>

--- trabalho_a4/core/models.py
+++ trabalho_a4/core/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Login(models.Model):
+    email=models.EmailField()
+    password=models.CharField(max_length=50)
+
+class Professor(models.Model):
+    nome=models.CharField()
+
+class Disciplina(models.Model):
+    codigo=models.IntegerField()
+    nome = models.CharField(max_length=100)
+    professores=models.ManyToManyField(Professor)
+    conteudo=[models.FileField]
+    cargaHoraria=models.IntegerField()
+    horario=models.DateField()
+
+class Aluno(models.Model):
+    matricula=models.IntegerField()
+    cpf=models.IntegerField()
+    nome=models.CharField(max_length=200)
+    endereco=models.CharField(max_length=400)
+    telefone=models.IntegerField()
+    dataAdmissao=models.DateField()
+    turmaCod=models.IntegerField()
+    disciplinas=models.ManyToManyField(Disciplina)
+    login=models.OneToOneField(Login)
--- trabalho_a4/core/views.py
+++ trabalho_a4/core/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'core/index.html')
+
+def adminHome(request):
+    return render(request,'core/admin.html')
+
+def alunoHome(request):
+    return render(request,'core/aluno.html')
+
+def professorHome(request):
+    return render(request,'core/professor.html')
