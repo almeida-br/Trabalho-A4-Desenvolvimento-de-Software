6464e93efaa28171592f35d7017caf8ec686a13d
samara <samaracostad9@gmail.com>
2024-06-15 11:59:19 -0300

    Classes de model.py atualizadas
    Co-authored-by: Kau002 <Kau002@users.noreply.github.com>

--- trabalho_a4/core/models.py
+++ trabalho_a4/core/models.py
@@ -12,8 +12,24 @@ class Login(models.Model):
     password=models.CharField(max_length=50)
     type=models.CharField(max_length=4,choices=AccessType.choices)
 
+class Endereco(models.Model):
+    logradouro=models.CharField(max_length=100)
+    numero=models.IntegerField()
+    bairro=models.CharField(max_length=100)
+    uf=models.CharField(max_length=2)
+
 class Professor(models.Model):
+    matricula=models.IntegerField(max_length=10,primary_key=True,default="0000000000")
+    cpf=models.IntegerField(max_length=11, default="00000000000")
     nome=models.CharField(max_length=50)
+    endereco=models.OneToOneField(Endereco,on_delete=models.CASCADE,default=("n/a",00,"n/a","00"))
+    telefone=models.IntegerField(max_length=11,default=00000000000)
+    cursosMinistrados=[models.CharField]
+    nivelEscolaridade=models.CharField
+    formacao=[models.CharField]
+    dataContratacao=models.DateField
+    disciplinasMinistradas=[models.CharField]
+    login=models.ForeignKey(Login,on_delete=models.CASCADE,default=00)    
 
 class Disciplina(models.Model):
     codigo=models.IntegerField(primary_key=True)
@@ -24,12 +40,19 @@ class Disciplina(models.Model):
     horario=models.DateField()
 
 class Aluno(models.Model):
-    matricula=models.IntegerField(primary_key=True)
-    cpf=models.IntegerField()
+    matricula=models.IntegerField(max_length=10,primary_key=True,default="0000000000")
+    cpf=models.IntegerField(max_length=11, default="00000000000")
     nome=models.CharField(max_length=200)
-    endereco=models.CharField(max_length=400)
-    telefone=models.IntegerField()
+    endereco=models.OneToOneField(Endereco,on_delete=models.CASCADE,default=("n/a",00,"n/a","00"))
+    telefone=models.IntegerField(max_length=11,default=00000000000)
     dataAdmissao=models.DateField(auto_now_add=True)
     turmaCod=models.IntegerField()
     disciplinas=models.ManyToManyField(Disciplina)
-    login=models.ForeignKey(Login,on_delete=models.CASCADE,default=("00","null","null","n/a"))
+    login=models.ForeignKey(Login,on_delete=models.CASCADE,default=00)
+
+class Turma(models.Model):
+    codigo=models.IntegerField(max_length=10)
+    periodo=models.IntegerField(max_length=2)
+    sala=models.CharField(max_length=20)
+    dataAbertura=models.DateField()
+    quantidadeAlunos=models.IntegerField(max_length=4)
